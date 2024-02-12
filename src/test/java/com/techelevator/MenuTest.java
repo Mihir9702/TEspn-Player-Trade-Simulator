eeed5c89963acf8436f1bab3e02b7c40d3dc9328
Mihir9702 <mkpatel9702@gmail.com>
2024-02-11 22:54:12 -0500

    fixed menu test

--- src/test/java/com/techelevator/MenuTest.java
+++ src/test/java/com/techelevator/MenuTest.java
@@ -3,9 +3,40 @@ package com.techelevator;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class MenuTest {
+
+  @Test
+  public void testPlayerInitialization() {
+    int jerseyNumber = 25;
+    String firstName = "Brandon";
+    String lastName = "Carlo";
+    String position = "Defense";
+    double capSpace = 4100000.00;
+    Map<String, Integer> stats = new HashMap<>();
+    stats.put("Goals", 61);
+    stats.put("Assists", 78);
+
+    // Act
+    Player player = new Player(
+      jerseyNumber,
+      firstName,
+      lastName,
+      position,
+      capSpace,
+      stats
+    );
+
+    // Assert
+    assertEquals(jerseyNumber, player.getJerseyNumber());
+    assertEquals(position, player.getPosition());
+    assertEquals(capSpace, player.getCapSpace());
+    assertEquals(stats, player.getStats());
+    assertEquals("Brandon Carlo", player.getName());
+  }
 
   @Test
   public void testGetFiles() {
